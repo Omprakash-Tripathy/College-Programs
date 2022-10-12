fd8024289d00c1328cc9bc6e6d41d202e350edc7
Omprakash Tripathy <tomprakash2407@gmail.com>
2022-10-12 13:28:56 +0530

    y

--- 3rd Semester DSA/12-10-2022/Prog4.c
+++ 3rd Semester DSA/12-10-2022/Prog4.c
@@ -43,6 +43,24 @@ void pop()
     }
 }
 
+void display()
+{
+    struct node *temp;
+    if(top == NULL)
+    {
+        printf("Stack is empty");
+    }
+    else
+    {
+        temp = top;
+        while(temp != NULL)
+        {
+            printf("The stack is %d /n",temp->data);
+            temp = temp->next;
+        }
+    }
+}
+
 void reverse()
 {
     struct node *temp;
@@ -66,7 +84,7 @@ int main()
     int choice, x;
     while(1)
     {
-        printf(" 1. Push\n 2. Pop\n 3. Reverse\n 4. Exit\n");
+        printf(" 1. Push\n 2. Pop\n 3. Display\n 4. Reverse\n 5. Exit\n");
         printf("Enter your choice: ");
         scanf("%d",&choice);
         switch(choice)
@@ -77,9 +95,11 @@ int main()
                     break;
             case 2: pop();
                     break;
-            case 3: reverse();
+            case 3: display();
                     break;
-            case 4: exit(0);
+            case 4: reverse();
+                    break;
+            case 5: exit(0);
             default: printf("Invalid choice");
         }
     }
