a46c84f52432ff6d31085948d9c677ff5ed1a9f8
Omprakash Tripathy <tomprakash2407@gmail.com>
2023-02-16 13:30:26 +0530

    o

---
+++ 4th Semester WT/Jav16Feb1.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class Jav16Feb1
+{
+public static void  main(String[] args)
+    {
+    
+        System.out.println("Name: Omprakash Tripathy");
+        System.out.println("Father's Name: Ashoke Kumar Tripathy");
+        System.out.println("Date of birth: 24/07/2003");
+        System.out.println("Blood Group: O+ve");
+        System.out.println("Aadhar Card No.:6414 9462 3095");
+        System.out.println("State: Odisha");
+        System.out.println("Mobile No: 9827299912");
+        System.out.println("Address: Flat no 204, SS Arcade Apartment, Sailashreevihar, Bhubaneswar");
+        System.out.println("Language Proficiency: English, Hindi, Odia, Sanskrit");
+        System.out.println("Academic Qualifications: "); 
+        System.out.println("B.Tech - Kalinga Institute of Industrial Technology");
+        System.out.println("12th - D.A.V. Public School - 95.6%");
+        System.out.println("10th - D.A.V. Public School - 99.0%");
+        System.out.println("Areas of Intrest : Stocks, Real Estate, Psychology");  
+    }
+}
